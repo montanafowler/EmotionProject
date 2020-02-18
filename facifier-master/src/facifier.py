49494fe667c6d18a82fe7fd94ecca3a3baea368b
montanafowler@yahoo.com <montanafowler@yahoo.com>
2020-02-17 21:00:38 -0800

    going to merge in neutral art

--- facifier-master/src/facifier.py
+++ facifier-master/src/facifier.py
@@ -9,10 +9,12 @@ from flask import Flask, request
 from markupsafe import escape
 from flask import render_template
 app = Flask(__name__)
-
+import base64
+import csv
 
 
 ESC = 27
+INDEX_DICT = {'happy':0, 'sad':0, 'angry':0, 'afraid':0, 'neutral':0, 'surprised':0, 'disgust':0}
 
 def start_webcam(model_emotion, model_gender, window_size, window_name='live', update_time=50):
     cv2.namedWindow(window_name, WINDOW_NORMAL)
@@ -55,7 +57,7 @@ def analyze_picture(file_name, model_emotion, model_gender, path, window_size, w
     #if window_size:
         #width, height = window_size
         #cv2.resizeWindow(window_name, width, height)
-
+    emotionsDetected = []
     image = cv2.imread(path, 1)
     for normalized_face, (x, y, w, h) in find_faces(image):
         emotion_prediction = model_emotion.predict(normalized_face)
@@ -65,15 +67,34 @@ def analyze_picture(file_name, model_emotion, model_gender, path, window_size, w
         else:
             cv2.rectangle(image, (x,y), (x+w, y+h), (255,0,0), 2)
         cv2.putText(image, emotions[emotion_prediction[0]], (x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,255), 2)
-    #cv2.imshow(window_name, image)
-    print("showed image")
+        print(emotions[emotion_prediction[0]])
+        emotionsDetected.append(emotions[emotion_prediction[0]])
+    
     savedImgPath = "../data/sample/output/"
+    savedOutputImgPath = savedImgPath + "output.jpg"
     savedImgPath += file_name
     cv2.imwrite(savedImgPath, image)
     print("wrote image to " + savedImgPath)
+    cv2.imwrite(savedOutputImgPath, image)
+    print("wrote image to " + savedOutputImgPath)
+    imagesToReturn = [savedOutputImgPath]
+    
+    for e in emotionsDetected:
+        csvFilename = e + "Art.csv"
+        with open(csvFilename, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            imgURL = list(reader)[INDEX_DICT[e]]["ImageURL"]
+            print(imgURL)
+            INDEX_DICT[e] += 1 #increment the index of the art
+            imagesToReturn.append(imgURL)
+
+    #cv2.imshow(window_name, image)
+    
+    
     #key = cv2.waitKey(0)
     #if key == ESC:
         #cv2.destroyWindow(window_name)
+    return imagesToReturn
 
 #@app.route('/facifier/')
 def initializeHomePage():
@@ -124,10 +145,19 @@ def startFacifier():
                 print("path to image: " + path)
                 if os.path.isfile(path):
                     print("calling analyze picture")
-                    analyze_picture(file_name, fisher_face_emotion, fisher_face_gender, path, window_size=(1280, 720), window_name=window_name)
+                    imagesToReturn = analyze_picture(file_name, fisher_face_emotion, fisher_face_gender, path, window_size=(1280, 720), window_name=window_name)
+                    #data = emotionsDetectedImg
+                    #print("emotionsDetectedImg " + str(emotionsDetectedImg))
+                    
+                    with open(imagesToReturn[0], "rb") as image_file:
+                        img = image_file.read()
+                        data = base64.b64encode(img)
+                    artImages = imagesToReturn[1:] #remove the output path
+                    print("imagesToReturn " + str(imagesToReturn))
                 else:
                     print("File not found!")
+                    return render_template("index.html")
     else:
         print("Invalid input, exiting program.")
-    return render_template("index.html")
+    return render_template("index.html", data=data.decode('utf8'), artImages=artImages)
 
